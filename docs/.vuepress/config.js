c6656530aa72c8d68ea725854b7738dc545500d5
storyxc <andresiniesta@foxmail.com>
2021-04-07 22:44:01 +0800

    pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,15 @@ module.exports = {
     head: [
         ['link', { rel: 'icon', href: '/assets/img/favicon.ico' }],
         ['meta', { name: 'author', content: '故事' }],
-        ['meta', { name: 'keywords', content: 'Java,Python,Vue,VuePress,后端开发' }]
+        ['meta', { name: 'keywords', content: 'Java,Python,Vue,VuePress,后端开发' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', href: '/icons/152x152.png' }],
+        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/icons/144.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     ],
     plugins: [
         [
@@ -18,6 +26,14 @@ module.exports = {
                     return moment(timestamp).format("lll")
                 }
             }
+        ],
+        ['@vuepress/pwa', {
+            serviceWorker: true,
+            updatePopup: {
+                message: "New content is available.",
+                buttonText: "Refresh"
+            }
+        }
         ]
     ],
     themeConfig: {
