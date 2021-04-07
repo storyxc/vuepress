df5d67b71b80b2afec5e6b09b479356c705ef2d5
storyxc <andresiniesta@foxmail.com>
2021-04-08 00:47:17 +0800

    auto sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,6 @@
 const headConf = require("./config/headConf")
 const pluginsConf = require("./config/pluginsConf")
 const navConf = require("./config/navConf")
-const sidebarConf = require("./config/sidebarConf")
 module.exports = {
     title: '故事',
     description: '故事的vuepress博客',
@@ -10,7 +9,6 @@ module.exports = {
     themeConfig: {
         logo: '/assets/img/story.jpg',
         lastUpdated: 'Last Updated',
-        nav: navConf,
-        sidebar: sidebarConf
+        nav: navConf
     }
 }
