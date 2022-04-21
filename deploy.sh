a0d5fc7f9bd9398376c90ce069bc29ff58bb15b1
storyxc <storyxc@163.com>
2022-04-21 19:12:56 +0800

    测试自动部署

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,28 @@
 set -e
 
 # 生成静态文件
-yarn build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
- echo 'blog.storyxc.com' > CNAME
+echo 'blog.storyxc.com' > CNAME
+
+if [ -z "${GITHUB_TOKEN}" ]; then
+  echo "GITHUB_TOKEN is not set"
+  exit 1
+else
+  msg='github actions自动部署'
+  githubUrl=https://storyxc:${GITHUB_TOKEN}@github.com/storyxc/vuepress.git
+  git config --global user.name "storyxc"
+  git config --global user.email "storyxc@163.com"
+fi
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "${msg}"
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:storyxc/vuepress.git master:gh-pages
+git push -f $githubUrl master:gh-pages
 
 cd -
